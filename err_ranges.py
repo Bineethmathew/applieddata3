d54cd351cb009e426814c7ecf0d9bab2c1adb0f4
Bineeth mathew <bineethmathew10@gmail.com>
2023-05-12 01:27:56 +0530

    Add files via upload

---
+++ err_ranges.py
@@ -0,0 +1,32 @@
+def err_ranges(x, func, param, sigma):
+    """
+    Calculates the upper and lower limits for the function, parameters and
+    sigmas for single value or array x. Functions values are calculated for
+    all combinations of +/- sigma and the minimum and maximum is determined.
+    Can be used for all number of parameters and sigmas >=1.
+
+    This routine can be used in assignment programs.
+    """
+
+    import itertools as iter
+    import numpy as np
+
+    # initiate arrays for lower and upper limits
+    lower = func(x, *param)
+    upper = lower
+
+    uplow = []   # list to hold upper and lower limits for parameters
+    for p,s in zip(param, sigma):
+        pmin = p - s
+        pmax = p + s
+        uplow.append((pmin, pmax))
+
+    pmix = list(iter.product(*uplow))
+
+    for p in pmix:
+        y = func(x, *p)
+        lower = np.minimum(lower, y)
+        upper = np.maximum(upper, y)
+
+    return lower, upper
+
